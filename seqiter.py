77502b636755fe4de0fa8c8cc806faf1453d10ad
Andrew Barnert <abarnert@yahoo.com>
2015-10-02 16:49:04 -0700

    Better demonstration of silliness

--- seqiter.py
+++ seqiter.py
@@ -1,9 +1,9 @@
 from collections.abc import Sequence, Iterator
 
 class SeqIter(Sequence, Iterator):
-    def __init__(self, elts):
+    def __init__(self, elts, _pos=-1):
         self._elts = list(elts)
-        self._pos = -1
+        self._pos = _pos
     def __next__(self):
         self._pos += 1
         try:
@@ -17,11 +17,14 @@ class SeqIter(Sequence, Iterator):
     def __len__(self):
         return len(self._elts)
     def __repr__(self):
-        return '{}({})'.format(type(self).__name__, self._elts)
+        return '{}({}, _pos={})'.format(
+            type(self).__name__, self._elts, self._pos)
 
 s = SeqIter([1, 2, 3, 4])
 print(s[0], s)
 print('nexting:', next(s))
+print(s[0], s)
+print('listing:', list(s))
 print(s[0], s)
 print('listing:', list(s))
 print(s[0], s)
